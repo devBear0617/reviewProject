e107dfa550e5b4477f9fc6476af8f6b9a2a741ff
user2 <gkfgran5037@naver.com>
2019-01-22 10:29:39 +0900

    test위한 주석처리, test.jsp, testController.java 추가
    servlet-context.xml - mapper 연동부분 주석
    root-context.xml - mapper 연동부분 주석
    test.jsp, testController.java 추가

---
+++ reviewProject/src/main/java/com/project/review/TestController.java
@@ -0,0 +1,19 @@
+package com.project.review;
+
+import java.util.Locale;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class TestController {
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		int testNum = 1234;
+		
+		model.addAttribute("testNum", testNum );
+		
+		return "test";
+	}
+}
