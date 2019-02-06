8c0277c971e7bec641f47d49fc85a9f0613e5f31
devBear0617 <devBear0617@gmail.com>
2019-02-06 16:47:08 +0900

    수정된 db에 맞게 VO 수정 및 추가

--- reviewProject/src/main/java/com/project/review/vo/BoardVO.java
+++ reviewProject/src/main/java/com/project/review/vo/BoardVO.java
@@ -4,77 +4,41 @@ import java.util.Date;
 
 public class BoardVO {
 	
-	private int boardnum;
-	private String boardname;
-	private Date boarddate;
-	private Date boardupdate;
-	private String boardcount;
-	private String boardcontent;
-	private int like_count;
-	private int com_count;
-	private String id;
+	private int BOARD_NUM;
+	private String BOARD_TITLE;
+	private String BOARD_CONTENT;
+	private Date BOARD_DATE;
 	
 	
-	public int getBoardnum() {
-		return boardnum;
+	public int getBOARD_NUM() {
+		return BOARD_NUM;
 	}
-	public void setBoardnum(int boardnum) {
-		this.boardnum = boardnum;
+	public void setBOARD_NUM(int bOARD_NUM) {
+		BOARD_NUM = bOARD_NUM;
 	}
-	public String getBoardname() {
-		return boardname;
+	public String getBOARD_TITLE() {
+		return BOARD_TITLE;
 	}
-	public void setBoardname(String boardname) {
-		this.boardname = boardname;
+	public void setBOARD_TITLE(String bOARD_TITLE) {
+		BOARD_TITLE = bOARD_TITLE;
 	}
-	public Date getBoarddate() {
-		return boarddate;
+	public String getBOARD_CONTENT() {
+		return BOARD_CONTENT;
 	}
-	public void setBoarddate(Date boarddate) {
-		this.boarddate = boarddate;
+	public void setBOARD_CONTENT(String bOARD_CONTENT) {
+		BOARD_CONTENT = bOARD_CONTENT;
 	}
-	public Date getBoardupdate() {
-		return boardupdate;
+	public Date getBOARD_DATE() {
+		return BOARD_DATE;
 	}
-	public void setBoardupdate(Date boardupdate) {
-		this.boardupdate = boardupdate;
-	}
-	public String getBoardcount() {
-		return boardcount;
-	}
-	public void setBoardcount(String boardcount) {
-		this.boardcount = boardcount;
-	}
-	public String getBoardcontent() {
-		return boardcontent;
-	}
-	public void setBoardcontent(String boardcontent) {
-		this.boardcontent = boardcontent;
-	}
-	public int getLike_count() {
-		return like_count;
-	}
-	public void setLike_count(int like_count) {
-		this.like_count = like_count;
-	}
-	public int getCom_count() {
-		return com_count;
-	}
-	public void setCom_count(int com_count) {
-		this.com_count = com_count;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
+	public void setBOARD_DATE(Date bOARD_DATE) {
+		BOARD_DATE = bOARD_DATE;
 	}
 	
 	@Override
 	public String toString() {
-		return "BoardVO [boardnum=" + boardnum + ", boardname=" + boardname + ", boarddate=" + boarddate
-				+ ", boardupdate=" + boardupdate + ", boardcount=" + boardcount + ", boardcontent=" + boardcontent
-				+ ", like_count=" + like_count + ", com_count=" + com_count + ", id=" + id + "]";
+		return "BoardVO [BOARD_NUM=" + BOARD_NUM + ", BOARD_TITLE=" + BOARD_TITLE + ", BOARD_CONTENT=" + BOARD_CONTENT
+				+ ", BOARD_DATE=" + BOARD_DATE + "]";
 	}
 	
 	
---
+++ reviewProject/src/main/java/com/project/review/vo/hashtagVO.java
@@ -0,0 +1,56 @@
+package com.project.review.vo;
+
+public class hashtagVO {
+	
+	private String HASHTAG1;
+	private String HASHTAG2;
+	private String HASHTAG3;
+	private String HASHTAG4;
+	private String HASHTAG5;
+	private String HASHTAG6;
+	
+	public String getHASHTAG1() {
+		return HASHTAG1;
+	}
+	public void setHASHTAG1(String hASHTAG1) {
+		HASHTAG1 = hASHTAG1;
+	}
+	public String getHASHTAG2() {
+		return HASHTAG2;
+	}
+	public void setHASHTAG2(String hASHTAG2) {
+		HASHTAG2 = hASHTAG2;
+	}
+	public String getHASHTAG3() {
+		return HASHTAG3;
+	}
+	public void setHASHTAG3(String hASHTAG3) {
+		HASHTAG3 = hASHTAG3;
+	}
+	public String getHASHTAG4() {
+		return HASHTAG4;
+	}
+	public void setHASHTAG4(String hASHTAG4) {
+		HASHTAG4 = hASHTAG4;
+	}
+	public String getHASHTAG5() {
+		return HASHTAG5;
+	}
+	public void setHASHTAG5(String hASHTAG5) {
+		HASHTAG5 = hASHTAG5;
+	}
+	public String getHASHTAG6() {
+		return HASHTAG6;
+	}
+	public void setHASHTAG6(String hASHTAG6) {
+		HASHTAG6 = hASHTAG6;
+	}
+	
+	@Override
+	public String toString() {
+		return "hashtagVO [HASHTAG1=" + HASHTAG1 + ", HASHTAG2=" + HASHTAG2 + ", HASHTAG3=" + HASHTAG3 + ", HASHTAG4="
+				+ HASHTAG4 + ", HASHTAG5=" + HASHTAG5 + ", HASHTAG6=" + HASHTAG6 + "]";
+	}
+	
+	
+}
