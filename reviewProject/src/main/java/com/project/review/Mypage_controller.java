ebcf9424937d7252483f046d32fe5cc8d68039a1
devBear0617 <devbear0617@gmail.com>
2019-04-01 13:38:18 +0900

    수정

--- reviewProject/src/main/java/com/project/review/Mypage_controller.java
+++ reviewProject/src/main/java/com/project/review/Mypage_controller.java
@@ -48,9 +48,9 @@ public class Mypage_controller {
 	@Autowired
 	private MemberService memberService;
 	
-	// eamil ID,PW | smtp 설정 필수!
-	final String emailSender = "chun6153"; 
-	final String emailPW = "cjsdnd!573"; 
+	// eamil ID(@naver.com 빼고 작성),PW | smtp 설정 필수!
+	final String emailSender = ""; 
+	final String emailPW = ""; 
 	
 //---------------------------------------------------------------
 	
